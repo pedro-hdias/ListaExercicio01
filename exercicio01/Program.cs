ea5994a1dbcf82501755ba4880fe637946787ba3
pedro-hdias <83011148+pedro-hdias@users.noreply.github.com>
2021-11-23 13:42:07 -0300

    Pequenas modificações.

--- exercicio01/Program.cs
+++ exercicio01/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+
+namespace Exercicio01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Olá!\nVou calcular sua nota, por favor, digite suas notas a seguir.\nDigite a nota 1: ");
+            string? tN01 = Console.ReadLine();
+            Console.WriteLine("Agora, digite a nota 2: ");
+            string? tN2 = Console.ReadLine();
+            double n01 = Convert.ToDouble(tN01);
+            double n02 = Convert.ToDouble(tN2);
+            double result = (n01 + n02) / 2;
+            if (result == 10)
+            {
+                Console.WriteLine("Você se formou com distinção! Sua nota foi: " + result);
+            }
+            else if (result <= 7)
+            {
+                Console.WriteLine("Você não passou, sua nota foi: " + result);
+            }
+            else
+            {
+                Console.WriteLine("Você passou, sua nota foi: " + result);
+            }
+        }
+    }
+}
