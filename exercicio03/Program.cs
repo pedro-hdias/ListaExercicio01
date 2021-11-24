1b8a2960200c715f51a1791845a12d38c53f284a
pedro-hdias <83011148+pedro-hdias@users.noreply.github.com>
2021-11-24 10:24:27 -0300

    Pequenas melhorias.

--- exercicio03/Program.cs
+++ exercicio03/Program.cs
@@ -5,11 +5,11 @@ public class program
     public static void Main(string[] args)
     {
         Console.WriteLine("Olá.\nVou te ajudar à escolher o melhor produto.\nInforme os valores de 3 produtos para que eu possa te ajudar à decidir.\nProduto 1: ");
-        string product01 = Console.ReadLine();
+        string? product01 = Console.ReadLine();
         Console.WriteLine("Produto 2: ");
-        string product02 = Console.ReadLine();
+        string? product02 = Console.ReadLine();
         Console.WriteLine("Produto 3: ");
-        string product03 = Console.ReadLine();
+        string? product03 = Console.ReadLine();
 
         double product1 = Convert.ToDouble(product01);
         double product2 = Convert.ToDouble(product02);
