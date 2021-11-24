9a3b96ac9a90905edf07f435858067bd147d80d2
pedro-hdias <83011148+pedro-hdias@users.noreply.github.com>
2021-11-24 10:24:49 -0300

    Exercício 04

---
+++ exercicio04/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Seja bem-vindo ao calculo de reajuste salarial.\nMe diga qual o seu salário.\nLembrando quê:\nSSalários até R$ 2800,00 ajuste de 20%;\nSSalários entre R$ 2800,00 e R$ 7000,00: aumento de 15%;\nSalários entre R$ 7000,00 e R$ 15000,00: aumento de 10%;\nE salários acima de R$ 15000,00 almento de 5%.\nDigite seu salário.");
+        string? wage = Console.ReadLine();
+        double Wage = Convert.ToDouble(wage);
+
+        if (Wage <= 2800.00)
+        {
+            double encrease = Wage * 0.20;
+            Console.WriteLine("O seu almento é de 20%. Seu salário de R$ " + Wage + "terá um acrécimo de R$ " + encrease);
+        }
+        else if (Wage <= 7000.00)
+        {
+            double encrease = Wage * 0.15;
+            Console.WriteLine("O seu almento é de 15%. Seu salário de R$ " + Wage + "terá um acrécimo de R$ " + encrease);
+        }
+        else if (Wage <= 15000.00)
+        {
+            double encrease = Wage * 0.10;
+            Console.WriteLine("O seu almento é de 10%. Seu salário de R$ " + Wage + "terá um acrécimo de R$ " + encrease);
+        }
+        else
+        {
+            double encrease = Wage * 0.05;
+            Console.WriteLine("O seu almento é de 5%. Seu salário de R$ " + Wage + "terá um acrécimo de R$ " + encrease);
+        }
+    }
+}
