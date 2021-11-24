d6360d9b1b661b2c3d16bead75f8bb663246cd71
pedro-hdias <83011148+pedro-hdias@users.noreply.github.com>
2021-11-24 14:43:26 -0300

    Exercícios 06

---
+++ exercicio06/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um número de 1 a 7 para saber o dia da semana.");
+        short number = Convert.ToInt16(Console.ReadLine());
+        switch (number)
+        {
+            case 1:
+                Console.WriteLine("Segunda-Feira");
+                break;
+            case 2:
+                Console.WriteLine("terça-feira");
+                break;
+            case 3:
+                Console.WriteLine("Quarta-feira");
+                break;
+            case 4:
+                Console.WriteLine("Quinta-feira");
+                break;
+            case 5:
+                Console.WriteLine("Sexta-feira");
+                break;
+            case 6:
+                Console.WriteLine("Sábado");
+                break;
+            case 7:
+                Console.WriteLine("Domingo");
+                break;
+            default:
+                Console.WriteLine("numero invalido, tente novamente");
+                break;
+        }
+    }
+}
