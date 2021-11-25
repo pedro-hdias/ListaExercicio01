7cfecbbe2c150d457dceb65aad0261ef7a4ec944
pedro-hdias <83011148+pedro-hdias@users.noreply.github.com>
2021-11-25 08:11:53 -0300

    Pois é, exercício10 antes do 09, porque é mais fácil, :D

---
+++ exercicio10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Me diga um número e eu vou dizer se é par ou ímpar.");
+        long number = Convert.ToInt64(Console.ReadLine());
+        long result = number % 2;
+        if (result == 0)
+        {
+            Console.WriteLine($"O número {number} é par.");
+        }
+        else
+        {
+            Console.WriteLine($"O número {number} é ímpar.");
+        }
+    }
+}
